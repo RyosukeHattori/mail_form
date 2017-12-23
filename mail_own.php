1a3866f1cf8467697ed37e894e2dd924c85e73c6
Ryosuke Hattori <oriel.tennisu@gmail.com>
2017-12-23 16:33:32 +0900

    使えない
    明らかに、mailtoの方が便利。
    自身のホームページ載せるのはやめたが、せっかく作ったのでサークルの方で使っている。

---
+++ mail_own.php
@@ -0,0 +1,486 @@
+<?php header("Content-Type:text/html;charset=utf-8"); ?>
+<?php //error_reporting(E_ALL | E_STRICT);
+
+if (version_compare(PHP_VERSION, '5.1.0', '>=')) {
+	date_default_timezone_set('Asia/Tokyo');
+}
+
+$site_top = "http://www.teamfreedom.club.uec.ac.jp/profile.html";
+
+// 管理者メールアドレス 
+$to = "oriel.tennisu@gmail.com";
+
+$Email = "Email";
+
+
+$Referer_check = 0;
+
+
+
+
+$userMail = 1;
+
+
+
+$subject = "問い合わせ";
+
+$confirmDsp = 1;
+
+
+$jumpPage = 0;
+
+
+$requireCheck = 1;
+
+$require = array('お名前','Email');
+
+
+//----------------------------------------------------------------------
+//  自動返信メール設定(START)
+//----------------------------------------------------------------------
+
+
+$remail = 1;
+
+$refrom_name = "Ryosuke Hattori";
+
+$re_subject = "問い合わせありがとうございます。Thank you for sending me a letter.";
+$dsp_name = 'お名前';
+
+//自動返信メール
+$remail_text = <<< TEXT
+
+
+お問い合わせありがとうございました。
+
+このメールを受け取った時点でメールの送信に成功しています。
+
+24時間以内にメールを返させていただきますので、少々お待ちください。
+
+Thank you for sending me a letter.
+
+Your message was successfully sent.
+
+I could you reply to you within 24 hours.
+
+
+送信内容は以下になります。
+
+TEXT;
+
+
+$mailFooterDsp = 0;
+
+
+$mail_check = 1;
+
+$hankaku = 1;
+
+
+$hankaku_array = array('電話番号','Email');
+
+
+
+
+//----------------------------------------------------------------------
+//  関数実行、変数初期化
+//----------------------------------------------------------------------
+$encode = "UTF-8";//このファイルの文字コード定義（変更不可）
+
+if(isset($_GET)) $_GET = sanitize($_GET);
+if(isset($_POST)) $_POST = sanitize($_POST);//NULLバイト除去//
+if(isset($_COOKIE)) $_COOKIE = sanitize($_COOKIE);//NULLバイト除去//
+if($encode == 'SJIS') $_POST = sjisReplace($_POST,$encode);
+$funcRefererCheck = refererCheck($Referer_check,$Referer_check_domain);
+
+//変数初期化
+$sendmail = 0;
+$empty_flag = 0;
+$post_mail = '';
+$errm ='';
+$header ='';
+
+if($requireCheck == 1) {
+	$requireResArray = requireCheck($require);//必須チェック実行し返り値を受け取る
+	$errm = $requireResArray['errm'];
+	$empty_flag = $requireResArray['empty_flag'];
+}
+//メールアドレスチェック
+if(empty($errm)){
+	foreach($_POST as $key=>$val) {
+		if($val == "confirm_submit") $sendmail = 1;
+		if($key == $Email) $post_mail = h($val);
+		if($key == $Email && $mail_check == 1 && !empty($val)){
+			if(!checkMail($val)){
+				$errm .= "<p class=\"error_messe\">【".$key."】is not correct e-mail format</p>\n";
+				$empty_flag = 1;
+			}
+		}
+	}
+}
+  
+if(($confirmDsp == 0 || $sendmail == 1) && $empty_flag != 1){
+	
+	//差出人に届くメール
+	if($remail == 1) {
+		$userBody = mailToUser($_POST,$dsp_name,$remail_text,$mailFooterDsp,$mailSignature,$encode);
+		$reheader = userHeader($refrom_name,$to,$encode);
+		$re_subject = "=?iso-2022-jp?B?".base64_encode(mb_convert_encoding($re_subject,"JIS",$encode))."?=";
+	}
+	//管理者宛に届くメール
+	$adminBody = mailToAdmin($_POST,$subject,$mailFooterDsp,$mailSignature,$encode,$confirmDsp);
+	$header = adminHeader($userMail,$post_mail,$BccMail,$to);
+	$subject = "=?iso-2022-jp?B?".base64_encode(mb_convert_encoding($subject,"JIS",$encode))."?=";
+	
+	mail($to,$subject,$adminBody,$header);
+	if($remail == 1 && !empty($post_mail)) mail($post_mail,$re_subject,$userBody,$reheader);
+}
+else if($confirmDsp == 1){ 
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>確認画面</title>
+<style type="text/css">
+
+#formWrap {
+	width:700px;
+	margin:0 auto;
+	color:#555;
+	line-height:120%;
+	font-size:90%;
+}
+table.formTable{
+	width:100%;
+	margin:0 auto;
+	border-collapse:collapse;
+}
+table.formTable td,table.formTable th{
+	border:1px solid #ccc;
+	padding:10px;
+}
+table.formTable th{
+	width:30%;
+	font-weight:normal;
+	background:#efefef;
+	text-align:left;
+}
+p.error_messe{
+	margin:5px 0;
+	color:red;
+}
+</style>
+</head>
+<body>
+
+<div id="formWrap">
+<?php if($empty_flag == 1){ ?>
+<div align="center">
+<h4>入力エラー/Input Error</h4>
+<p align="center">入力を確認してください。Please check the input and resend.</p>
+
+<?php echo $errm; ?><br /><br /><input type="button" value=" 前画面に戻る " onClick="history.back()">
+</div>
+<?php }else{ ?>
+<h3>確認画面</h3>
+<p align="center">以下の内容で間違いがなければ、「送信する」ボタンを押してください。</p>
+<form action="<?php echo h($_SERVER['SCRIPT_NAME']); ?>" method="POST">
+<table class="formTable">
+<?php echo confirmOutput($_POST);//入力内容を表示?>
+</table>
+<p align="center"><input type="hidden" name="mail_set" value="confirm_submit">
+<input type="hidden" name="httpReferer" value="<?php echo h($_SERVER['HTTP_REFERER']);?>">
+<input type="submit" value="　送信する　">
+<input type="button" value="前画面に戻る" onClick="history.back()"></p>
+</form>
+<?php } ?>
+</div><!-- /formWrap -->
+
+</body>
+</html>
+<?php
+
+}
+
+if(($jumpPage == 0 && $sendmail == 1) || ($jumpPage == 0 && ($confirmDsp == 0 && $sendmail == 0))) { 
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109516473-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-109516473-1');
+</script>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>完了画面/Success</title>
+</head>
+<body>
+<div align="center">
+<?php if($empty_flag == 1){ ?>
+<h4>入力にエラーがあります。下記をご確認の上「戻る」ボタンにて修正をお願い致します。</h4>
+<div style="color:red"><?php echo $errm; ?></div>
+<br /><br /><input type="button" value=" 前画面に戻る " onClick="history.back()">
+</div>
+</body>
+</html>
+<?php }else{ ?>
+送信ありがとうございました。<br />
+送信は正常に完了しました。<br /><br />
+<a href="<?php echo $site_top ;?>">トップページへ戻る&raquo;</a>
+</div>
+</body>
+</html>
+
+
+//----------------------------------------------------------------------
+//  関数定義(START)
+//----------------------------------------------------------------------
+function checkMail($str){
+	$mailaddress_array = explode('@',$str);
+	if(preg_match("/^[\.!#%&\-_0-9a-zA-Z\?\/\+]+\@[!#%&\-_0-9a-z]+(\.[!#%&\-_0-9a-z]+)+$/", "$str") && count($mailaddress_array) ==2){
+		return true;
+	}else{
+		return false;
+	}
+}
+function h($string) {
+	global $encode;
+	return htmlspecialchars($string, ENT_QUOTES,$encode);
+}
+function sanitize($arr){
+	if(is_array($arr)){
+		return array_map('sanitize',$arr);
+	}
+	return str_replace("\0","",$arr);
+}
+//Shift-JISの場合に誤変換文字の置換関数
+function sjisReplace($arr,$encode){
+	foreach($arr as $key => $val){
+		$key = str_replace('＼','ー',$key);
+		$resArray[$key] = $val;
+	}
+	return $resArray;
+}
+//送信メールにPOSTデータをセットする関数
+function postToMail($arr){
+	global $hankaku,$hankaku_array;
+	$resArray = '';
+	foreach($arr as $key => $val) {
+		$out = '';
+		if(is_array($val)){
+			foreach($val as $key02 => $item){ 
+				//連結項目の処理
+				if(is_array($item)){
+					$out .= connect2val($item);
+				}else{
+					$out .= $item . ', ';
+				}
+			}
+			$out = rtrim($out,', ');
+			
+		}else{ $out = $val; }//チェックボックス（配列）追記ここまで
+		if(get_magic_quotes_gpc()) { $out = stripslashes($out); }
+		
+		//全角→半角変換
+		if($hankaku == 1){
+			$out = zenkaku2hankaku($key,$out,$hankaku_array);
+		}
+		if($out != "confirm_submit" && $key != "httpReferer") {
+			$resArray .= "【 ".h($key)." 】 ".h($out)."\n";
+		}
+	}
+	return $resArray;
+}
+//確認画面の入力内容出力用関数
+function confirmOutput($arr){
+	global $hankaku,$hankaku_array;
+	$html = '';
+	foreach($arr as $key => $val) {
+		$out = '';
+		if(is_array($val)){
+			foreach($val as $key02 => $item){ 
+				//連結項目の処理
+				if(is_array($item)){
+					$out .= connect2val($item);
+				}else{
+					$out .= $item . ', ';
+				}
+			}
+			$out = rtrim($out,', ');
+			
+		}else{ $out = $val; }//チェックボックス（配列）追記ここまで
+		if(get_magic_quotes_gpc()) { $out = stripslashes($out); }
+		$out = nl2br(h($out));//※追記 改行コードを<br>タグに変換
+		$key = h($key);
+		
+		//全角→半角変換
+		if($hankaku == 1){
+			$out = zenkaku2hankaku($key,$out,$hankaku_array);
+		}
+		
+		$html .= "<tr><th>".$key."</th><td>".$out;
+		$html .= '<input type="hidden" name="'.$key.'" value="'.str_replace(array("<br />","<br>"),"",$out).'" />';
+		$html .= "</td></tr>\n";
+	}
+	return $html;
+}
+
+//全角→半角変換
+function zenkaku2hankaku($key,$out,$hankaku_array){
+	global $encode;
+	if(is_array($hankaku_array) && function_exists('mb_convert_kana')){
+		foreach($hankaku_array as $hankaku_array_val){
+			if($key == $hankaku_array_val){
+				$out = mb_convert_kana($out,'a',$encode);
+			}
+		}
+	}
+	return $out;
+}
+//配列連結の処理
+function connect2val($arr){
+	$out = '';
+	foreach($arr as $key => $val){
+		if($key === 0 || $val == ''){//配列が未記入（0）、または内容が空のの場合には連結文字を付加しない（型まで調べる必要あり）
+			$key = '';
+		}elseif(strpos($key,"円") !== false && $val != '' && preg_match("/^[0-9]+$/",$val)){
+			$val = number_format($val);//金額の場合には3桁ごとにカンマを追加
+		}
+		$out .= $val . $key;
+	}
+	return $out;
+}
+
+//管理者宛送信メールヘッダ
+function adminHeader($userMail,$post_mail,$BccMail,$to){
+	$header = '';
+	if($userMail == 1 && !empty($post_mail)) {
+		$header="From: $post_mail\n";
+		if($BccMail != '') {
+		  $header.="Bcc: $BccMail\n";
+		}
+		$header.="Reply-To: ".$post_mail."\n";
+	}else {
+		if($BccMail != '') {
+		  $header="Bcc: $BccMail\n";
+		}
+		$header.="Reply-To: ".$to."\n";
+	}
+		$header.="Content-Type:text/plain;charset=iso-2022-jp\nX-Mailer: PHP/".phpversion();
+		return $header;
+}
+//管理者宛送信メールボディ
+function mailToAdmin($arr,$subject,$mailFooterDsp,$mailSignature,$encode,$confirmDsp){
+	$adminBody="「".$subject."」からメールが届きました\n\n";
+	$adminBody .="＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝\n\n";
+	$adminBody.= postToMail($arr);//POSTデータを関数からセット
+	$adminBody.="\n＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝\n";
+	$adminBody.="送信された日時：".date( "Y/m/d (D) H:i:s", time() )."\n";
+	$adminBody.="送信者のIPアドレス：".@$_SERVER["REMOTE_ADDR"]."\n";
+	$adminBody.="送信者のホスト名：".getHostByAddr(getenv('REMOTE_ADDR'))."\n";
+	if($confirmDsp != 1){
+		$adminBody.="問い合わせのページURL：".@$_SERVER['HTTP_REFERER']."\n";
+	}else{
+		$adminBody.="問い合わせのページURL：".@$arr['httpReferer']."\n";
+	}
+	if($mailFooterDsp == 1) $adminBody.= $mailSignature;
+	return mb_convert_encoding($adminBody,"JIS",$encode);
+}
+
+//ユーザ宛送信メールヘッダ
+function userHeader($refrom_name,$to,$encode){
+	$reheader = "From: ";
+	if(!empty($refrom_name)){
+		$default_internal_encode = mb_internal_encoding();
+		if($default_internal_encode != $encode){
+			mb_internal_encoding($encode);
+		}
+		$reheader .= mb_encode_mimeheader($refrom_name)." <".$to.">\nReply-To: ".$to;
+	}else{
+		$reheader .= "$to\nReply-To: ".$to;
+	}
+	$reheader .= "\nContent-Type: text/plain;charset=iso-2022-jp\nX-Mailer: PHP/".phpversion();
+	return $reheader;
+}
+//ユーザ宛送信メールボディ
+function mailToUser($arr,$dsp_name,$remail_text,$mailFooterDsp,$mailSignature,$encode){
+	$userBody = '';
+	if(isset($arr[$dsp_name])) $userBody = h($arr[$dsp_name]). " 様\n";
+	$userBody.= $remail_text;
+	$userBody.="\n＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝\n\n";
+	$userBody.= postToMail($arr);//POSTデータを関数からセット
+	$userBody.="\n＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝\n\n";
+	$userBody.="送信日時：".date( "Y/m/d (D) H:i:s", time() )."\n";
+	if($mailFooterDsp == 1) $userBody.= $mailSignature;
+	return mb_convert_encoding($userBody,"JIS",$encode);
+}
+//必須チェック関数
+function requireCheck($require){
+	$res['errm'] = '';
+	$res['empty_flag'] = 0;
+	foreach($require as $requireVal){
+		$existsFalg = '';
+		foreach($_POST as $key => $val) {
+			if($key == $requireVal) {
+				
+				//連結指定の項目（配列）のための必須チェック
+				if(is_array($val)){
+					$connectEmpty = 0;
+					foreach($val as $kk => $vv){
+						if(is_array($vv)){
+							foreach($vv as $kk02 => $vv02){
+								if($vv02 == ''){
+									$connectEmpty++;
+								}
+							}
+						}
+						
+					}
+					if($connectEmpty > 0){
+						$res['errm'] .= "<p class=\"error_messe\">【".h($key)."】は必須項目です。</p>\n";
+						$res['empty_flag'] = 1;
+					}
+				}
+				//デフォルト必須チェック
+				elseif($val == ''){
+					$res['errm'] .= "<p class=\"error_messe\">【".h($key)."】は必須項目です。</p>\n";
+					$res['empty_flag'] = 1;
+				}
+				
+				$existsFalg = 1;
+				break;
+			}
+			
+		}
+		if($existsFalg != 1){
+				$res['errm'] .= "<p class=\"error_messe\">【".$requireVal."】が未選択です。</p>\n";
+				$res['empty_flag'] = 1;
+		}
+	}
+	
+	return $res;
+}
+//リファラチェック
+function refererCheck($Referer_check,$Referer_check_domain){
+	if($Referer_check == 1 && !empty($Referer_check_domain)){
+		if(strpos($_SERVER['HTTP_REFERER'],$Referer_check_domain) === false){
+			return exit('<p align="center">リファラチェックエラー。フォームページのドメインとこのファイルのドメインが一致しません</p>');
+		}
+	}
+}
+function copyright(){
+	echo '<a style="display:block;text-align:center;margin:15px 0;font-size:11px;color:#aaa;text-decoration:none" href="http://www.php-factory.net/" target="_blank">- PHP工房 -</a>';
+}
+//----------------------------------------------------------------------
+//  関数定義(END)
+//----------------------------------------------------------------------
+?>
